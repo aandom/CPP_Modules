adfd65f09e611c29a0864f539e4d84173954acbd
alexashie <alexashieandom@gmail.com>
2023-10-20 16:45:11 +0400

    newnew

---
+++ CPP_09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aandom <aandom@student.abudhabi42.ae>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/19 10:31:07 by aandom            #+#    #+#             */
+/*   Updated: 2023/10/19 10:31:07 by aandom           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+# include <iostream>
+# include <algorithm>
+
+
+# endif
